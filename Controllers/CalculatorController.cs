cb4430d55e7779e28b1a1cb050527cfe7f84575a
Nadia Martinez <a2024112081@estudiantes.upsa.edu.bo>
2025-08-20 19:56:11 -0400

    creación del endpoint

--- Controllers/CalculatorController.cs
+++ Controllers/CalculatorController.cs
@@ -1,12 +1,18 @@
 using Microsoft.AspNetCore.Http; 
 using Microsoft.AspNetCore.Mvc;
 
-namespace WebApplication1.Controllers;
+namespace WebApplication1.Controllers
 
 {
     [Route("api/[controller]")]
     [ApiController]
     public class CalculatorController : ControllerBase
     {
+        [HttpGet("sum")]
+        public IActionResult GetSum([FromQuery] int a, [FromQuery] int b)
+        {
+            var result = a + b;
+            return Ok(new { a, b, result});
+        }
     }
 }
